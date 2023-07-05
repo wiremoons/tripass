b446b788898d81a2de2c5cc328d11de41ac74682
Simon Rowe <wiremoons@users.noreply.github.com>
2023-07-05 22:33:01 +0100

    Update 'build.bat' - improve 'submodule' sync commands

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 git submodule init
-git submodule update
 git pull --recurse-submodules
+git submodule update --remote --merge
 rem Use command below in Powershel 7.x directly if prefered:
 rem odin build . -define:BUILDTS="$(date)" -strict-style -vet
 odin build . -define:BUILDTS="%date%" -strict-style -vet
